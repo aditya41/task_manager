de52cf3de7366a6ea06239724200e1492e1848af
aditya41 <aditya41@users.noreply.github.com>
2020-12-25 17:42:13 +0530

    authentication

--- src/index.js
+++ src/index.js
@@ -5,6 +5,10 @@ const taskRouter = require('./routes/tasks')
 const app = express()
 const port = process.env.PORT || 3000
 
+// app.use((req, res, next) => {
+//     res.status(503).send('maintainance buddy')
+// })
+
 app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
@@ -14,12 +18,12 @@ app.listen(port, () => {
     console.log('Server is up on port ' + port)
 })
 
-const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 
 const myFunction = async() => {
-    const password = 'Red12345'
-    const hashedPassword = await bcrypt.hash(password, 8)
-    console.log(hashedPassword)
-    const isMatch = await bcrypt.compare('Red12345', hashedPassword)
+    const token = jwt.sign({ _id: 'abc' }, 'thisismynewcourse', { expiresIn: '7 days' })
+    console.log(token)
+    const data = jwt.verify(token, 'thisismynewcourse')
+    console.log(data)
 }
 myFunction()
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const { update } = require('../models/user')
+const auth = require('../middlewares/auth')
 const User = require('../models/user')
 const router = new express.Router()
 
-router.get('/users', async(req, res) => {
+router.get('/users', auth, async(req, res) => {
 
     try {
         const users = await User.find({})
@@ -20,12 +21,16 @@ router.get('/users', async(req, res) => {
 router.post('/users/login', async(req, res) => {
     try {
         const user = await User.findByCredentials(req.body.email, req.body.password)
-        res.send(user)
+        const token = await user.generateAuthToken()
+        res.send({ user, token })
     } catch (e) {
         res.status(400).send()
     }
 })
 
+router.get('/users/me', auth, async(req, res) => {
+    res.send(req.user)
+})
 router.get('/users/:id', async(req, res) => {
     // console.log(req.params)
     const _id = req.params.id
@@ -74,7 +79,8 @@ router.post('/users', async(req, res) => {
 
     try {
         await user.save()
-        res.status(201).send(user)
+        const token = await user.generateAuthToken()
+        res.status(201).send({ user, token })
     } catch (e) {
         res.status(400).send(e)
     }
